43bfe440664c5d1368f52c91afd27e17d15216ca
Pannawit <pannawitandsirirote@gmail.com>
2026-01-16 18:14:40 +0700

    Add date_diff functionality

--- date.py
+++ date.py
@@ -14,7 +14,23 @@ def day_of_year(day, month, year):
 def day_in_year(year):
     return 366 if is_leap_year(year) else 365
 
+def date_diff(date1, date2):
+    x = [int(i) for i in date1.split("-")] 
+    y = [int(i) for i in date2.split("-")] 
 
+    day1 = day_of_year(x[0], x[1], x[2])
+    day2 = day_of_year(y[0], y[1], y[2])
+
+    if x[2] == y[2]:
+        return day2 - day1 + 1
+    else:
+        count_year = [i for i in range(x[2] + 1, y[2])]
+        year_day = 0
+        for i in count_year:
+            year_day += day_in_year(i)
+            
+        remaining_days_year1 = day_in_year(x[2]) - day1
+        return remaining_days_year1 + year_day + day2 + 1
 
 month_list = (31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31)
 try:
@@ -37,4 +53,4 @@ if (is_leap_year(z[2]) == False and z[1] == 2 and z[0]>28) or (is_leap_year(z[5]
 
     print("Invalid")
     exit()
-
+print(date_diff(x[0],x[1]))
